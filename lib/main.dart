f362817a856ad3404302ea367ac57dc062a7abf6
BB Caspian <casfian@gmail.com>
2021-08-19 10:02:48 +0800

    added functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; 
+import 'package:http/http.dart' as http;
 //kita jadikan sebagai object supaya mudah guna
- 
+
 void main() => runApp(MyApp());
- 
+
 //root class
 class MyApp extends StatelessWidget {
   @override
@@ -17,19 +19,32 @@ class MyApp extends StatelessWidget {
 
 //secondary class
 class Home extends StatelessWidget {
-Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  List posts = [];
+
+  //kita akan guna data dari URL ne
+  //data source
+  var url = Uri.parse('https://jsonplaceholder.typicode.com/photos');
+
+  //buat function
+  Future getPosts() async {
+    var data = await http.get(url);
+    var jsonData = json.decode(data.body); //data akan diterjemah ke JSON
+    print(jsonData);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      );
+      ),
+    );
   }
 }
